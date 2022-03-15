b6e41e972e47a8279f8f35ce07998c88fb6a8efb
JRF_moontasirul <moontasirul@jrfintel.com>
2022-03-15 12:33:04 +0600

    rename string

--- app/src/main/java/com/wealthPark/testApplication/utils/Constants.kt
+++ app/src/main/java/com/wealthPark/testApplication/utils/Constants.kt
@@ -2,7 +2,7 @@ package com.wealthPark.testApplication.utils
 
 class Constants {
     companion object {
-        const val PREFERENCE_NAME = "wunder_pref"
+        const val PREFERENCE_NAME = "wealth_park_pref"
         const val TIMESTAMP_FORMAT = "yyyyMMdd_HHmmss"
         const val ASSESS_TOKEN = "df7c313b47b7ef87c64c0f5f5cebd6086bbb0fa"
     }
